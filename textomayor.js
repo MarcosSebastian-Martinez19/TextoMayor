a998ea2f2b8756dc1180e4e58e8a92365a587e6a
Marcos Martinez <marcossebastian429ssebastian429@gmail.com>
2022-07-02 22:42:20 -0300

    Texto con mayor cantidad de caracteres

---
+++ textomayor.js
@@ -0,0 +1,10 @@
+let valores = [true, 5, false, "hola", "adios", 2];
+
+let string1 = valores[3];
+let string2 = valores[4];
+
+if (string1.length > string2.length) {
+    console.log("El texto con mayor caracteres es: " + string1);
+} else {
+    console.log("El texto con mayor caracteres es: " + string2);
+};
